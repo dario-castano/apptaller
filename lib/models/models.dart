55ddf0b47a2a3180762d251465de979affa86624
Dario <dario.castano@aim.com>
2022-12-02 18:01:26 -0500

    Add model export file

---
+++ lib/models/models.dart
@@ -0,0 +1,6 @@
+export 'package:apptaller/models/album.dart';
+export 'package:apptaller/models/comment.dart';
+export 'package:apptaller/models/photo.dart';
+export 'package:apptaller/models/post.dart';
+export 'package:apptaller/models/todo.dart';
+export 'package:apptaller/models/user.dart';
