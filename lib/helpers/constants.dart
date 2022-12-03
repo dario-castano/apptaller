43e3843c1c4fe2b5c95f550370668e3eeb310a99
Dario <dario.castano@aim.com>
2022-12-03 07:28:38 -0500

    Add constants

---
+++ lib/helpers/constants.dart
@@ -0,0 +1,8 @@
+class Constants {
+  static const baseURL = "jsonplaceholder.typicode.com";
+  static const albumsPath = "albums";
+  static const commentsPath = "comments";
+  static const postsPath = "posts";
+  static const todosPath = "todos";
+  static const usersPath = "users";
+}
